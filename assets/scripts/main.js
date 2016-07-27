945a6dd868321af1a9fedcde252625a49c47b8cb
NatasaPeic <peic.natasa@gmail.com>
2016-07-26 22:26:34 -0400

    Add updated main.js
    - working on the for loop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,64 +9,111 @@
 
 
 
-const winner = function () {
-  for (let i = 1; i <= 3; i++) {
 
-    // let domId = '#R' + i + 'C1';
-    // console.log(domId);
+          const winner = function () {
+            for (let i = 1; i <= 3; i++) {
 
-    if($("#R" + i + "C1").text() === "X" && $("#R" + i + "C1").text() === $("#R" + i + "C2").text() &&
-    $("#R" + i + "C1").text() === $("#R" + i + "C3").text()){
-
-      alert("X won!");
-
-  } else if ($("#R" + i + "C1").text() === "O" && $("#R" + i + "C1").text() === $("#R" + i + "C2").text() &&
-  $("#R" + i + "C1").text() === $("#R" + i + "C3").text()) {
-
-    alert("O won!");
-
-  }
+              // let domId = '#R' + i + 'C1';
+              // console.log(domId);
 
 
 
-  }
+              //horizontally X
+              if($("#R" + i + "C1").text() === "X" && $("#R" + i + "C1").text() === $("#R" + i + "C2").text() &&
+              $("#R" + i + "C1").text() === $("#R" + i + "C3").text()){
+
+                alert("X won!");
 
 
-};
+              // horizontally O
+              } else if ($("#R" + i + "C1").text() === "O" && $("#R" + i + "C1").text() === $("#R" + i + "C2").text() &&
+              $("#R" + i + "C1").text() === $("#R" + i + "C3").text()) {
 
+                alert("O won!");
+
+
+              // vertically X
+            } else if ($("#R1" + "C" + i).text() === "X" && $("#R1" + "C" + i).text() === $("#R2" + "C" + i).text() &&
+              $("#R1" + "C" + i).text() === $("#R3" + "C" + i).text()) {
+
+                alert("X won!");
+
+              } else if ($("#R1" + "C" + i).text() === "O" && $("#R1" + "C" + i).text() === $("#R2" + "C" + i).text() &&
+                $("#R1" + "C" + i).text() === $("#R3" + "C" + i).text()) {
+
+                  alert("O won!");
+
+              }
+
+            }
+
+            // diagonally
+            if($("#R1C1").text() === "X" &&  $("#R1C1").text() === $("#R2C2").text() && $("#R1C1").text() === $("#R3C3").text()){
+
+              alert("X won!");
+
+            } else if ($("#R1C1").text() === "O" &&  $("#R1C1").text() === $("#R2C2").text() && $("#R1C1").text() === $("#R3C3").text()) {
+
+              alert("O won!");
+            }
+
+
+            if($("#R1C3").text() === "X" &&  $("#R1C3").text() === $("#R2C2").text() && $("#R1C3").text() === $("#R3C1").text()){
+
+              alert("X won!");
+
+            } else if ($("#R1C3").text() === "O" &&  $("#R1C3").text() === $("#R2C2").text() && $("#R1C3").text() === $("#R3C1").text()) {
+
+              alert("O won!");
+            }
+
+
+          };
 
 
 
     let turn = "O";
 
-    $("#R1C1, #R1C2, #R1C3, #R2C1, #R2C2, #R2C3, #R3C1, #R3C2, #R3C3")
+    $("#R1C1, #R1C2, #R1C3, #R2C1, #R2C2, #R2C3, #R3C1, #R3C2, #R3C3").click(function(){
 
-    .click(
+      let cell = $(this);
 
-      function(){
+      if(turn === "O"){
 
-  let cell = $(this);
+            turn = "X";
 
-  if(turn === "O"){
+            cell.text("X");
+            cell.val("X");
+                 $(this).off();
 
-      turn = "X";
+                 winner();
 
-      cell.text("X");
-      cell.val("X");
-         $(this).off();
+      } else {
+            turn = "O";
 
-    winner();
-
-    } else {
-         turn = "O";
             cell.text("O");
             cell.val("O");
             $(this).off();
 
-              winner();
+                 winner();
           }
         }
     );
+
+
+
+$("#reset-button").click(function () {
+
+        $("#R1C1, #R1C2, #R1C3, #R2C1, #R2C2, #R2C3, #R3C1, #R3C2, #R3C3").empty();
+        $("#R1C1, #R1C2, #R1C3, #R2C1, #R2C2, #R2C3, #R3C1, #R3C2, #R3C3").val("");
+
+
+  });
+
+
+
+
+
 
 
 
